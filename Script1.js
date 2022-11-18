37d669a4b3a57403955b5c4ce945f313d5353e3d
Hasnain <hasnainrao786000@gmail.com>
2022-11-18 20:39:59 +0500

    object

--- Script1.js
+++ Script1.js
@@ -1,11 +1,24 @@
 // JavaScript variable
-let name = 'Hasnain'; //string
-let age = '22'; //number
-let number = 163.5; //number
-let a = false; //boolean
-let lastname = null; // clear storage null
-
-console.log(name, age, number, a);
+//let name = 'Hasnain'; //string
+//let age = '22'; //number
+//let number = 163.5; //number
+//let a = false; //boolean
+//let lastname = null; // clear storage null
+//console.log(name, age, number, a);
 // Constant
-const price = 5000;
-console.log(price);
+//const price = 5000;
+//console.log(price);
+
+// Object
+let Car = {
+    Company: 'Honda',
+        Colour: 'Red',
+    Model: '2022'
+}
+// dot notation
+Car.Company = 'audi';
+// bracket notation 
+let select = 'Model';
+Car[select] = 2023;
+// 
+console.log(Car);
