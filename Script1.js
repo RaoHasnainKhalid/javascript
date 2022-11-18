fe40e099496e5af2e35b87d487a151f8f60f3ba3
Hasnain <hasnainrao786000@gmail.com>
2022-11-18 20:20:56 +0500

    constant

--- Script1.js
+++ Script1.js
@@ -1,7 +1,11 @@
 // JavaScript variable
-let name = 'Hasnain';
-let age = '22';
-let number = 163.5;
-let a = false;
-console.log(name, age, number, a);
+let name = 'Hasnain'; //string
+let age = '22'; //number
+let number = 163.5; //number
+let a = false; //boolean
+let lastname = null; // clear storage null
 
+console.log(name, age, number, a);
+// Constant
+const price = 5000;
+console.log(price);
