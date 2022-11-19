33a98bfce705e356167190b58d68aaa1184d47fa
Hasnain <hasnainrao786000@gmail.com>
2022-11-19 15:23:16 +0500

    Operator In JavaScript

--- Script1.js
+++ Script1.js
@@ -30,12 +30,73 @@
 
 //function
 
-function greet(name) {
-    console.log('Hasnain' + ' ' + name);
-}
-greet('Rao');
-// calculation
-function multiple(num) {
-    return num * num;
-}
-console.log(multiple(3));
+// function greet(name) {
+//     console.log('Hasnain' + ' ' + name);
+// }
+// greet('Rao');
+// // calculation
+// function multiple(num) {
+//     return num * num;
+// }
+// console.log(multiple(3));
+
+//Operator 
+// arithmatic +,-,*./
+//  let a = 5;
+//  let b = 7;
+// let sum = a + b;
+// let sub = a - b;
+// let mul = a * b;
+// let dvd = a / b;
+// console.log(sum,sub,mul,dvd);
+
+// // assignment operator ( = , +=)
+// let x = 5;
+// x += 2;
+// console.log(x);
+
+// // comparison operator (>,<,=>,<=)
+// let y = 5;
+// let z = 7;
+// console.log(y<z);
+// console.log(y>z);
+// console.log(y<=z);
+// console.log(y>=z);
+
+// // Equality Operator
+// let abc = 57;
+// //Sronge  Equality (type + value )
+
+// console.log(57 === 57);
+// console.log('57' === 57);
+
+// //Sronge  Equality (value )
+// console.log( '57' == 57);
+
+// // Ternary Operator 
+// let price = 6000;
+// let value = price>5000 ? 'Gold' : 'Silver';
+// console.log(value);
+
+// // Logical Operator ( && || !)
+// let ab = true;
+// let cd = false;
+// let ef = ab && cd;
+// let gh = !ef;
+// console.log(ef);
+// console.log(gh);
+// console.log(ab || cd);
+
+// Logical Operator With Non Boolen
+// falsy ( empty , False , 0 , null , NaN , undefined)
+// truthy
+ console.log(false || 'Hasnain'); 
+ console.log(false || 151);
+
+// Swapping 
+ let car = 'Red';
+ let bike = 'Black';
+ let color = car;
+ car = bike ;
+ bike = color;
+ console.log(car,bike);
