be8fb9cb5cdc9bc2ff443d21dc50ee1172bf39f9
Hasnain <hasnainrao786000@gmail.com>
2022-11-20 14:44:17 +0500

    If else and Switch Statement

--- Script1.js
+++ Script1.js
@@ -90,13 +90,55 @@
 // Logical Operator With Non Boolen
 // falsy ( empty , False , 0 , null , NaN , undefined)
 // truthy
- console.log(false || 'Hasnain'); 
- console.log(false || 151);
+//  console.log(false || 'Hasnain'); 
+//  console.log(false || 151);
 
-// Swapping 
- let car = 'Red';
- let bike = 'Black';
- let color = car;
- car = bike ;
- bike = color;
- console.log(car,bike);
+// // Swapping 
+//  let car = 'Red';
+//  let bike = 'Black';
+//  let color = car;
+//  car = bike ;
+//  bike = color;
+//  console.log(car,bike);
+
+// if else
+// 6am to 12pm "Good Morning"
+// 12pm to 6pm "Good Afternoon"
+// otherwise "Good Evening"
+  let hours = 19;
+  if (hours >=6 && hours < 12 ){
+    console.log("Good Morning");
+ }
+ else if(hours >=12 && hours < 18 )
+ console.log("Good Afternoon");
+ else 
+ console.log("Good Evening");
+
+// Switch Statment
+let day = '8';
+switch (day){
+    case('1'):
+    console.log("Monday");
+    break;
+    case('2'):
+    console.log("Tuesday");
+    break;
+    case('3'):
+    console.log("Wednesday");
+    break;
+    case('4'):
+    console.log("Thursday");
+    break;
+    case('5'):
+    console.log("Friday");
+    break;
+    case('6'):
+    console.log("Saturday");
+    break;
+    case('7'):
+    console.log("Sunady");
+    break;
+    default:
+    console.log("Invalid");
+}
+ 
